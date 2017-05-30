d1596edeb95d76c4f54943a371f755445a4438f7
nut <nut@buzzwoo.de>
2017-05-30 22:01:02 +0700

    update readme, welcome page, ignore `_ide_helper.php`

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Chiangmai Movies
                 </div>
 
                 <div class="links">
